944d16980a6d5100b8c5bddb7fa5e44593d78823
fre-ben <fschocke@gmail.com>
2021-02-01 16:56:11 +0100

    Add JS to form

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -1,9 +1,38 @@
+// export function createRegisterForm() {
+//     return `
+//      <form class="form"
+//          <h2>Reset Password</h2>
+//          <p>Please enter your email to receive a link to create a new password via email</p>
+//          <input placeholder="email" />
+//          <button>Submit</button>
+//       </form>
+//     `;
+// }
+
 export function createRegisterForm() {
-    return `
-     <form class="form"
-         <h2>Reset Password</h2>
-         <p>Please enter your email to receive a link to create a new password via email</p>
-         <input placeholder="email" />
-         <input type="submit />
-      </form>
-    `}
+    const form = document.createElement("form");
+    form.className = "form";
+
+    const paragraph = document.createElement("p");
+    paragraph.innerText = "Please enter your email to receive a link to create a new password via email";
+
+    const title = document.createElement("h2");
+    title.innerText = "Reset Password";
+
+    const input = document.createElement("input");
+    input.placeholder = "Email";
+
+    const button = document.createElement("button");
+    button.innerText = "Submit";
+
+
+    function register() {
+        alert("Submitted!");
+    };
+
+    button.addEventListener("click", register);
+
+    form.append(title, paragraph, input, button);
+
+    return form; 
+};
